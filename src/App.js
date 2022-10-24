eb6c326bd58e8733cc51f03cf0c056e312c7aa3e
abrarasif11 <faasif929@gmail.com>
2022-10-24 23:19:15 +0600

    route-setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './layout/Main';
 
 function App() {
  const router = createBrowserRouter([
-
+  {
+    path : '/',
+    element : <Main></Main>,
+    
+  }
  ])
   return (
     <div className="App">

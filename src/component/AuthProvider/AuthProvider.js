08cd05898de323fbb11b121c11bdd01d4368acfd
abrarasif11 <faasif929@gmail.com>
2022-10-25 22:44:43 +0600

    register-auth-add

--- src/component/AuthProvider/AuthProvider.js
+++ src/component/AuthProvider/AuthProvider.js
@@ -1,13 +1,19 @@
-import React, { createContext } from 'react';
-import {getAuth} from "firebase/auth"
+import React, { createContext, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider} from "firebase/auth"
 import app from '../../firebase/Firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app); 
 const AuthProvider = ({children}) => {
-    const user = {displayName : "Yennefer"};
-    
-    const authInfo = { user };
+    const [user , setUSer] = useState(null); 
+    // const user = {displayName : "Yennefer"};
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+    const createUSer = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password);
+    }
+    const authInfo = { user , createUSer };
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const Register = () => {
+  const [error, setError] = useState(null);
+
+  const { createUser } = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const htmlForm = event.target;
+    const userName = htmlForm.name.value;
+    const photoURL = htmlForm.photoURL.value;
+    const email = htmlForm.email.value;
+    const password = htmlForm.password.value;
+    
+    console.log(userName, photoURL, email, password);
+
+    createUser(email, password)
+    .then((result) =>{
+      const user = result.user;
+      htmlForm.reset();
+      setError('');
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+  }
     return (
       <section className="p-6 font-poppins dark:text-gray-100">
-      <form novalidate="" className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow dark:bg-gray-900 ng-untouched ng-pristine ng-valid">
+      <form onSubmit={handleSubmit}  className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow dark:bg-gray-900 ng-untouched ng-pristine ng-valid">
         <h2 className="w-full text-3xl font-bold leading-tight">Register Here...</h2>
         <div>
-          <label for="name" className="block mb-1 ml-1">Name</label>
-          <input id="name" type="text" placeholder="enter your name" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" required/>
-          <label for="name" className="block mb-1 ml-1">Photo URL</label>
-          <input id="name" type="text" placeholder="enter photo url" required="" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+          <label htmlFor="name" className="block mb-1 ml-1">Name</label>
+          <input id="name" name='name' type="text" placeholder="enter your name" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" required/>
+          <label htmlFor="name"  className="block mb-1 ml-1">Photo URL</label>
+          <input id="photoURL" name='photoURL' type="text" placeholder="enter photo url" required="" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
         </div>
         <div>
-          <label for="email" className="block mb-1 ml-1">Email</label>
-          <input id="email" type="email" placeholder="enter your email"  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"  />
+          <label htmlFor="email" className="block mb-1 ml-1">Email</label>
+          <input id="email" name='email' type="email" placeholder="enter your email"  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"  />
         </div>
         <div>
-          <label for="message" className="block mb-1 ml-1">Password</label>
-          <input id="password" type="password" placeholder="enter your password" className="mb-5 w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"></input>
+          <label htmlFor="message" className="block mb-1 ml-1">Password</label>
+          <input id="password" name='password' type="password" placeholder="enter your password" className="mb-5 w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"></input>
         </div>
-        <label for="password" className="text-sm"></label>
+        <label htmlFor="password" className="text-sm"></label>
               <Link rel="noopener noreferrer" to="/login" className="text-x hover:underline dark:text-gray-400">Already have an account Login?</Link>
         <div>
           <button type="submit" className="w-full px-4 py-2 font-bold rounded shadow focus:outline-none focus:ring hover:ring focus:ring-opacity-50 dark:bg-violet-400 focus:ring-violet-400  bg-black hover:bg-zinc-800 text-gray-200">Register</button>
